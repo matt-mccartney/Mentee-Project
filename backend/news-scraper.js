35407ac5fe3ffd46e2e560a5cf646dd22bd5c5c2
Matt McCartney <mlm071604@hotmail.com>
2024-02-10 15:56:57 -0500

    Added News Scraper

--- backend/news-scraper.js
+++ backend/news-scraper.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 
-async function getNewsSiteSource(url, articleSection, titleSelector) {
+async function getNewsSiteSource(url, articleSection, titleSelector, descriptionSelector, imageSelector) {
   let resp = await axios.get(url);
   if (!(resp.status < 400 && resp.status >= 200)) {
     throw new Error(
@@ -14,19 +14,27 @@ async function getNewsSiteSource(url, articleSection, titleSelector) {
   let articles = [];
   section.map((_, newsArticle) => {
     let title = parser(newsArticle).find(titleSelector).text();
-    articles.push(title)
+    if (title !== null || title !== "") {
+      articles.push({
+        "title": title,
+        "description": parser(newsArticle).find(descriptionSelector).text(),
+        "image_url": parser(newsArticle).find(imageSelector).attr().src
+      })
+    }
   });
   return articles
 }
 
-getNewsSiteSource(
+/* getNewsSiteSource(
   "https://theverge.com/tech",
   ".duet--content-cards--content-card",
   "h2"
-).then(resp => console.log(resp.toString()));
+).then(resp => console.log(resp.toString())); */
 
 getNewsSiteSource(
     "https://www.nytimes.com/section/technology",
     "article",
-    "a"
-  ).then(resp => console.log(resp.toString()));
+    "a",
+    "p",
+    "img"
+  ).then(resp => resp.map(x => console.log(x.title, "-", x.description, "-", x.image_url)));
